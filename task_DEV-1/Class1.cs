44b601d6796d01208f1f4dc037ecdaa9d5d47dcc
Andrysha <mnepoh1998@gmail.com>
2018-03-06 23:41:15 +0300

    ADD

---
+++ task_DEV-1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MaxNumberOfCharacters
+{
+    class FindMaxSequnceInString //finds the maximum subseqence of characters in the string
+    {
+        private int FindMaxSequnce(char[] str)
+        {
+            int max_length = 0;
+            int counter = 1;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    counter++;//increase the values of the counter ,if the symbols match
+                }
+                else
+                {
+                    counter = 1;//equate to zero if the symbols don't match
+                }
+                if (max_length < counter)
+                {
+                    max_length = counter;
+                }
+            }
+            return (max_length);
+        }
+        private void Result(int length) //output result's
+        {
+            if (length == 1)
+            {
+                Console.WriteLine("duplicate character not found");
+            }
+            else
+            {
+                Console.WriteLine(length);
+            }
+        }
+    }
+}
---
+++ task_DEV-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{   /// <summary>
+    /// Class MaxNumberOfCharacters
+    /// searches maximum number of same serial symbols in the string
+    /// and searches some exceptions
+    /// </summary>
+    class MaxNumberOfCharacters
+    {
+        public static void Main(string[] arg)
+        {
+            try
+            {
+                FindMaxSequnceInString example = new FindMaxSequnceInString; //create instance of a class
+                var str = arg[0].ToCharArray();
+                int length = example.FindMaxSequnceInString(str); //find concurrences
+                example.Result(length);
+            }
+            catch
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine("the function takes too many arguments");
+            }
+        }
+    }
+}
