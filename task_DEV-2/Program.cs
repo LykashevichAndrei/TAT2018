76f82381a9b413c73010b74dcb7f2aac88bc9053
Andrysha <mnepoh1998@gmail.com>
2018-03-06 23:44:19 +0300

    ADD

---
+++ task_DEV-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace task_DEV_2
+{ 
+    /// <summary>
+    /// The entry point to the program.
+    /// </summary>
+    class EnteryPoint
+    {
+        static void Main()
+        {
+                EvenElementsOfTheString mystring = new EvenElementsOfTheString(); 
+                string str = mystring.FindingEvenElemenstInString();  //We assign the even values ​​of the string to the variable
+            Console.WriteLine(str);
+        }
+    }
+}
+
