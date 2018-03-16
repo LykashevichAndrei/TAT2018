9ada0577e3812cee0dad29418ebb3cd392748fb0
Andrysha <mnepoh1998@gmail.com>
2018-03-16 14:28:02 +0300

    change

--- task_DEV-5/task3_test/TestClass.cs
+++ task_DEV-5/task3_test/TestClass.cs
@@ -1,15 +1,14 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using task_DEV_3;
 
-namespace UnitTestProject1
+namespace task_DEV_3
 {
     [TestFixture]
     public class UnitTest1
     {
         [TestCase(255, 16, "FF")]
-        [TestCase(10, 2, "101")]
-        [TestCase(20, 20, "1")]
+        [TestCase(10, 2, "0101")]
+        [TestCase(20, 20, "01")]
         public void calculatenewsystemtest(int number, int numbersystem, string result)
         {
             NewNumberSysteminterpreter test1 = new NewNumberSysteminterpreter(number, numbersystem);
@@ -17,12 +16,12 @@ namespace UnitTestProject1
             Assert.AreEqual(result, newstr);
         }
         [TestCase("1001", "1001")]
-        [TestCase("100", "1")]
+        [TestCase("100", "001")]
         [TestCase("abcd", "dcba")]
         public void ReverseAnswerTest(string input,string output)
         {
             NewNumberSysteminterpreter test2 = new NewNumberSysteminterpreter(10, 10);
-            string temp = ReverseAnswer(input);
+            string temp = test2.ReverseAnswer(input);
             Assert.AreEqual(temp, output);
         }
 
