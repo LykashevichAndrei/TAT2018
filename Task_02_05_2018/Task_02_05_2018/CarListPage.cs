8c026d85a6ad042e424bf6951f96cab6d296148e
Andrysha <mnepoh1998@gmail.com>
2018-05-08 18:35:29 +0300

    Add

---
+++ Task_02_05_2018/Task_02_05_2018/CarListPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_02_05_2018
+{
+    class CarListPage
+    {
+        private ChromeDriver driver;
+        public CarListPage(ChromeDriver Driver)
+        {
+            this.driver = Driver;
+        }
+        public ChromeDriver OpenCarList(IWebElement element)
+        {
+            element = driver.FindElementByXPath("//option[text()='BMW']");
+            element.Click();
+            element = driver.FindElementById("model_id");
+            element.Click();
+            element = driver.FindElementByXPath("//option[text()='M5']");
+            element.Click();
+            element = driver.FindElementById("submit_presearch");
+            element.Click();
+            return driver;
+        }
+        public void DeduceThePriceOfTheCheapestCar(IWebElement element)
+        {
+            element = driver.FindElementByXPath("//a[text()='цене']");
+            element.Click();
+            element = driver.FindElementByXPath("//small");
+            Console.WriteLine(element.Text);
+        }
+    }
+}
---
+++ Task_02_05_2018/Task_02_05_2018/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Task_02_05_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://av.by/");
+            IWebElement element = driver.FindElementByName("brand_id[]");
+            element.Click();
+            CarListPage FirstPage = new CarListPage(driver);
+            driver = FirstPage.OpenCarList(element);
+            Thread.Sleep(1000);
+            FirstPage.DeduceThePriceOfTheCheapestCar(element);
+        }
+    }
+}
