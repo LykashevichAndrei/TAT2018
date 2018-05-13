d7422dcc1a3c02169320ca48c3717c016cc1b019
Andrysha <mnepoh1998@gmail.com>
2018-05-13 23:30:06 +0300

    task11

---
+++ Task_DEV-11/ClientConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Task_DEV_11;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day,months,year;
+            do
+            {
+                day = Convert.ToInt32(Console.ReadLine());
+            }
+            while (day < 0 || day > 31);
+            do
+            {
+                months = Convert.ToInt32(Console.ReadLine());
+            }
+            while (months < 0 || months > 12);
+            do
+            {
+                year = Convert.ToInt32(Console.ReadLine());
+            }
+            while (year < 0);
+            Server server = new Server();
+            int days=server.PrintDaysFromChristsBirthday(year, months, day);
+            Console.WriteLine("amount of days="+days);
+        }
+    }
+}
---
+++ Task_DEV-11/NUnit.Tests1/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Task_DEV_11;
+
+namespace ClientConsole
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase(0, 0, 0, 0 )]
+        [TestCase(0, 1, 1, 32)]
+        [TestCase(1, 0, 1, 366)]
+        [TestCase(0, 1, 0, 31)]
+        public void CountThePastNumberOfDaysTest_ValidValues(int day, int month, int year, int result)
+        {
+            Server webServis = new Server();
+            int days = webServis.GetDaysFromChristsBirthday(year, month, day);
+            Assert.AreEqual(result,days);
+        }
+    }
+}
